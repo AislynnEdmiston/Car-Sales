1700f38a14106f2a7cdbf576adbdc1773bab694a
AislynnEdmiston <davidandaislynn@gmail.com>
2019-10-22 20:48:54 -0400

    removed store for now

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 // imports provider
 import { Provider } from 'react-redux';
-// import createStore 
-import createStore from './createReduxStore';
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-//creates store 
-const store = createStore();
+
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
