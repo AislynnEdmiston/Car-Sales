c69bf99ab6e1a9d9f5ba709aab586d98e39ec7c6
AislynnEdmiston <davidandaislynn@gmail.com>
2019-10-22 22:35:18 -0400

    added store, created reducer, added connect

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 // imports provider
 import { Provider } from 'react-redux';
-
+import { reducer } from './reducers/reducer'
+import { createStore } from 'redux';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-
+const store = createStore(reducer);
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
 // connects app to store through the Provider
-    <Provider>
+    <Provider store={store}>
         <App />
     </Provider>, rootElement);
