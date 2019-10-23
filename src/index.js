f8e70dadfbb72f19a0cf9b7a51b34199dd8b4a39
AislynnEdmiston <davidandaislynn@gmail.com>
2019-10-22 20:35:07 -0400

    set up provider and store in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+// imports provider
+import { Provider } from 'react-redux';
+// import createStore 
+import createStore from './createReduxStore';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+//creates store 
+const store = createStore();
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+// connects app to store through the Provider
+    <Provider store={store}>
+        <App />
+    </Provider>, rootElement);
